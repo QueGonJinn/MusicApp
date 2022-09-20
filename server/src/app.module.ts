e4ed84837b8c1b7560bafa5108390a6a75ee620a
QueGonJinn <molialow13@gmail.com>
2022-09-20 13:33:51 +0300

    feat: implementation REST API

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.qrrdh3v.mongodb.net/music-platform?retryWrites=true&w=majority',
+    ),
+    TrackModule,
+  ],
+})
 export class AppModule {}
